8893dfc00de425bfb3da98da99e31d8f8d0848aa
Ricardo Tucker <ricardo@Ricardos-iMac.wv.cc.cmu.edu>
2016-09-29 14:51:09 -0400

    Finished Angry_Mom prefab, set up basic backyard scene, implemented FPS controls

---
+++ Assets/_Scripts/Dart_Script.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dart_Script : MonoBehaviour {
+	private Rigidbody rb;
+	private Coroutine disappear;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		disappear = StartCoroutine (Disappear (3));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//TODO rotate with y-velocity of rigidbody
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.tag == "Enemy") {
+			StopCoroutine (disappear);
+			Destroy (this.gameObject);
+
+			Animator animator = col.gameObject.GetComponent<Animator> ();
+			animator.SetBool ("isDead", true);
+			animator.SetBool ("isWalking", false);
+			animator.SetBool ("isAttacking", false);
+		}
+	}
+
+	IEnumerator Disappear(float delay) {
+		yield return new WaitForSeconds (delay);
+		Destroy (this.gameObject);
+	}
+}
---
+++ Assets/_Scripts/Mom_Script.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mom_Script : MonoBehaviour {
+
+	public float walkSpeed;
+
+	private Animator animator;
+	private Transform player;
+
+	private bool dead = false;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		dead = animator.GetBool ("isDead");
+		if (!dead) {
+			Act ();
+		} else {
+			//Body disappears after 2 seconds
+			StartCoroutine (Disappear (2));
+		}
+	}
+
+	void Act() {
+		Vector3 direction = player.position - this.transform.position;
+		direction.y = 0; // prevents model from rotating up/down while turning towards player
+		this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.LookRotation (direction), 0.1f);
+
+		if (direction.magnitude < 2) {
+			//Enemy is close enough to attack
+			animator.SetBool ("isDead", false);
+			animator.SetBool ("isWalking", false);
+			animator.SetBool ("isAttacking", true);
+		} else {
+			//Enemy walks towards player to attack
+			this.transform.Translate (new Vector3 (0, 0, walkSpeed));
+			animator.SetBool ("isDead", false);
+			animator.SetBool ("isWalking", true);
+			animator.SetBool ("isAttacking", false);
+		}
+	}
+
+	IEnumerator Disappear(float delay) {
+		yield return new WaitForSeconds (delay);
+		Destroy (this.gameObject);
+	}
+}
